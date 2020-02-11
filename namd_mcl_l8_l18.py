5b24aaefd429aec3dd3c8bf722605b447417ceb3
Mateusz Bieniek <bieniekmat@gmail.com>
2020-02-11 18:35:07 +0000

    First sketch for preparation of the namd simulation.

---
+++ namd_mcl_l8_l18.py
@@ -0,0 +1,224 @@
+"""
+Load two ligands, run the topology superimposer, and then
+using the results, generate the NAMD input files.
+
+frcmod file format: http://ambermd.org/FileFormats.php#frcmod
+"""
+from topology_superimposer import get_atoms_bonds_from_mol2, superimpose_topologies, assign_coords_from_pdb
+import os
+import json
+import numpy as np
+
+def getSuptop(mol1, mol2):
+    # use mdanalysis to load the files
+    leftlig_atoms, leftlig_bonds, rightlig_atoms, rightlig_bonds, mda_l1, mda_l2 = \
+        get_atoms_bonds_from_mol2(mol1, mol2)
+
+    # assign
+    # fixme - Ideally I would reuse the mdanalysis data for this
+    startLeft = None
+    ligand1_nodes = {}
+    for atomNode in leftlig_atoms:
+        ligand1_nodes[atomNode.get_id()] = atomNode
+        if atomNode.atomName == 'O3':
+            startLeft = atomNode
+    for nfrom, nto, btype in leftlig_bonds:
+        ligand1_nodes[nfrom].bindTo(ligand1_nodes[nto], btype)
+
+    startRight = None
+    ligand2_nodes = {}
+    for atomNode in rightlig_atoms:
+        ligand2_nodes[atomNode.get_id()] = atomNode
+        if atomNode.atomName == 'O7':
+            startRight = atomNode
+    for nfrom, nto, btype in rightlig_bonds:
+        ligand2_nodes[nfrom].bindTo(ligand2_nodes[nto], btype)
+
+    suptops = superimpose_topologies(ligand1_nodes.values(), ligand2_nodes.values(),
+                                     starting_node_pairs=[(startLeft, startRight)])
+    assert len(suptops) == 1
+    return suptops[0], mda_l1, mda_l2
+
+
+def save_superimposition_results(filepath):
+    # fixme - check if the file exists
+    with open(filepath, 'w') as FOUT:
+        # use json format, only use atomNames
+        data = {
+                'matching': {str(n1): str(n2) for n1, n2 in suptop.matched_pairs},
+                'appearing': list(map(str, suptop.get_appearing_atoms())),
+                'disappearing': list(map(str, suptop.get_disappearing_atoms()))
+                }
+        FOUT.write(json.dumps(data, indent=4))
+
+
+def write_dual_top_pdb(filepath):
+    # fixme - find another library that can handle writing to a PDB file, mdanalysis?
+    # save the ligand with all the appropriate atomic positions, write it using the pdb format
+    # pdb file format: http://www.wwpdb.org/documentation/file-format-content/format33/sect9.html#ATOM
+    # write a dual .pdb file
+    with open(filepath, 'w') as FOUT:
+        for atom in mda_l1.atoms:
+            """
+            There is only one forcefield which is shared across the two topologies. 
+            Basically, we need to check whether the atom is in both topologies. 
+            If that is the case, then the atom should have the same name, and therefore appear only once. 
+            However, if there is a new atom, it should be specfically be outlined 
+            that it is 1) new and 2) the right type
+            """
+            # write all the atoms if they are matched, that's the common part
+            REMAINS = 0
+            if suptop.contains_left_atomName(atom.name):
+                line = f"ATOM  {atom.id:>5d} {atom.name:>4s} {atom.resname:>3s}  " \
+                       f"{atom.resid:>4d}    " \
+                       f"{atom.position[0]:>8.3f}{atom.position[1]:>8.3f}{atom.position[2]:>8.3f}" \
+                       f"{1.0:>6.2f}{REMAINS:>6.2f}" + (' ' * 11) + \
+                       '  ' + '  ' + '\n'
+                FOUT.write(line)
+            else:
+                # this atom was not found, this means it disappears, so we should update the
+                DISAPPEARING_ATOM = -1.0
+                line = f"ATOM  {atom.id:>5d} {atom.name:>4s} {atom.resname:>3s}  " \
+                       f"{atom.resid:>4d}    " \
+                       f"{atom.position[0]:>8.3f}{atom.position[1]:>8.3f}{atom.position[2]:>8.3f}" \
+                       f"{1.0:>6.2f}{DISAPPEARING_ATOM:>6.2f}" + \
+                       (' ' * 11) + \
+                       '  ' + '  ' + '\n'
+                FOUT.write(line)
+        # add atoms from the right topology,
+        # which are going to be created
+        for atom in mda_l2.atoms:
+            if not suptop.contains_right_atomName(atom.name):
+                APPEARING_ATOM = 1.0
+                line = f"ATOM  {atom.id:>5d} {atom.name:>4s} {atom.resname:>3s}  " \
+                       f"{atom.resid:>4d}    " \
+                       f"{atom.position[0]:>8.3f}{atom.position[1]:>8.3f}{atom.position[2]:>8.3f}" \
+                       f"{1.0:>6.2f}{APPEARING_ATOM:>6.2f}" + \
+                       (' ' * 11) + \
+                       '  ' + '  ' + '\n'
+                FOUT.write(line)
+
+
+def write_merged(suptop, merged_filename):
+    # recreate the mol2 file that is merged and contains the correct atoms from both
+    # mol2 format: http://chemyang.ccnu.edu.cn/ccb/server/AIMMS/mol2.pdf
+    with open(merged_filename, 'w') as FOUT:
+        bonds = suptop.getDualTopologyBonds()
+
+        FOUT.write('@<TRIPOS>MOLECULE ' + os.linesep)
+        # name of the molecule
+        FOUT.write('merged ' + os.linesep)
+        # num_atoms [num_bonds [num_subst [num_feat [num_sets]]]]
+        # fixme this is tricky
+        FOUT.write(f'{suptop.getUnqiueAtomCount():d} '
+                   f'{len(bonds):d}' + os.linesep)
+        # mole type
+        FOUT.write('SMALL ' + os.linesep)
+        # charge_type
+        FOUT.write('NO_CHARGES ' + os.linesep)
+        FOUT.write(os.linesep)
+
+        # write the atoms
+        FOUT.write('@<TRIPOS>ATOM ' + os.linesep)
+        # atom_id atom_name x y z atom_type [subst_id [subst_name [charge [status_bit]]]]
+        # e.g.
+        #       1 O4           3.6010   -50.1310     7.2170 o          1 L39      -0.815300
+
+        # so from the two topologies all the atoms are needed and they need to have a different atom_id
+        # so we might need to name the atom_id for them, other details are however pretty much the same
+        # the importance of atom_name is difficult to estimate
+
+        # we are going to assign IDs in the superimposed topology in order to track which atoms have IDs
+        # and which don't
+
+        subst_id = 1    # resid basically
+        # write all the atoms that were matched first with their IDs
+        # prepare all the atoms, note that we use primarily the left ligand naming
+        all_atoms = [left for left, right in suptop.matched_pairs] + suptop.get_unmatched_atoms()
+        # reorder the list according to the ID
+        all_atoms.sort(key=lambda atom: suptop.get_generated_atom_ID(atom))
+
+        for atom in all_atoms:
+            FOUT.write(f'{suptop.get_generated_atom_ID(atom)} {atom.atomName} '
+                       f'{atom.position[0]:.4f} {atom.position[1]:.4f} {atom.position[2]:.4f} '
+                       f'{atom.type.lower()} {subst_id} {atom.resname} {atom.charge:.6f} {os.linesep}')
+
+        # for left_atom, _ in suptop.matched_pairs:
+        #     # note that the atom id is the most important
+        #     FOUT.write(f'{suptop.get_generated_atom_ID(left_atom)} {left_atom.atomName} '
+        #                f'{left_atom.position[0]:.4f} {left_atom.position[1]:.4f} {left_atom.position[2]:.4f} '
+        #                f'{left_atom.type} {subst_id} {left_atom.resname} {left_atom.charge} {os.linesep}')
+
+        # write the IDs for the atoms which are appearing/disappearing
+        # for unmatched in suptop.get_unmatched_atoms():
+        #     FOUT.write(f'{suptop.get_generated_atom_ID(unmatched)} {unmatched.atomName} '
+        #                f'{unmatched.position[0]:.4f} {unmatched.position[1]:.4f} {unmatched.position[2]:.4f} '
+        #                f'{unmatched.type} {subst_id} {unmatched.resname} {unmatched.charge} {os.linesep}')
+
+        FOUT.write(os.linesep)
+
+        # write bonds
+        FOUT.write('@<TRIPOS>BOND ' + os.linesep)
+
+        # we have to list every bond:
+        # 1) all the bonds between the paired atoms, so that part is easy
+        # 2) bonds which link the disappearing atoms, and their connection to the paired atoms
+        # 3) bonds which link the appearing atoms, and their connections to the paired atoms
+
+        bond_counter = 1
+        list(bonds)
+        for bond_from_id, bond_to_id, bond_type in sorted(list(bonds)):
+            # Bond Line Format:
+            # bond_id origin_atom_id target_atom_id bond_type [status_bits]
+            FOUT.write(f'{bond_counter} {bond_from_id} {bond_to_id} {bond_type}' + os.linesep)
+            bond_counter += 1
+
+
+root_path = '/home/dresio/code/BAC2020/namd_study/mcl_l8_l18'
+# load the files and superimpose the two topologies
+# before you carry out the first step, you have to generate the .mol2
+# with bond and charge information, and ideally superimposed molecule?
+# fixme - call any of the tools you have (antechamber, parmchk2)
+suptop, mda_l1, mda_l2 = getSuptop(os.path.join(root_path, 'left.mol2'),
+                                   os.path.join(root_path, 'right.mol2'))
+# save the results of the topology superimposition as a json
+save_superimposition_results(os.path.join(root_path, 'left_right_meta_fep.json'))
+write_dual_top_pdb(os.path.join(root_path, 'left_right.pdb'))
+# save the merged topologies as a .mol2 file
+top_merged_filename = os.path.join(root_path, 'merged.mol2')
+write_merged(suptop, top_merged_filename)
+
+# solvate using namd, this means copying the leap.in script and using tleap
+
+# Generate the directory structure for all the lambdas, and copy the files
+for lambda_step in [0, 0.05] + list(np.linspace(0.1, 0.9, 9)) + [0.95, 1]:
+    lambda_path = os.path.join(root_path, f'lambda_{lambda_step:.2f}')
+    if not os.path.exists(lambda_path):
+        os.makedirs(lambda_path)
+
+    # for each lambda create 5 replicas
+    for replica_no in range(1, 5 + 1):
+        replica_dir = os.path.join(lambda_path, f'rep{replica_no}')
+        if not os.path.exists(replica_dir):
+            os.makedirs(replica_dir)
+
+        # copy the correct files to each of the directories
+        #
+        # todo
+print('hi')
+
+# todo copy the solvation script, as well as the ligand1, ligand2,
+# todo copy the little python script that will be used to udpate the solvated script
+# todo copy the EQ script that will be run
+# todo copy the lambda .fep file
+"""
+# todo - generate completely different directories with scripts with namd for each lambda
+# todo - use sqlite to synchronise the execution and managing of all of the simulations? (ie one major script)
+for example, imagine a script that says "do_ties" which knows that there is 13 x 5 different directories
+which have to be run each, and what it does, it goes into each of them, and schedules them, but 
+it first checks where the simulation is by looking up its little .db file, 
+ie lambda1.1 simulation has a db which is empty, so it schedules it to run, but lambda 1.2 has already finished, 
+and that's written in its DB, whereas lambda 1.3 has status "submitted" and therefore does not need to be 
+submitted again, whereas lambda 1.5 has status "running" which also can be ignored. etc etc
+"""
+
