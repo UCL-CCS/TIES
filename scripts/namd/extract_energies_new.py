f8a84231850bf514ea6981d7e2444dc339674441
bieniekmat <bieniekmat@gmail.com>
2020-03-20 15:02:52 +0000

    Refactoring and adding more observables.

--- scripts/namd/extract_energies_new.py
+++ scripts/namd/extract_energies_new.py
@@ -15,37 +15,11 @@ from pymbar import timeseries
 from collections import OrderedDict
 
 
-def choder_get_eqpart(datapoints):
-    [t0, g, Neff_max] = timeseries.detectEquilibration(datapoints)
-    return datapoints[t0:]
+def extract_energies(location):
+    """
+    @location - referes to the main locations where the lambda directories reside
+    """
 
-
-def autocorr(x):
-    result = np.correlate(x, x, mode='full')
-    return result[int(result.size / 2):]
-
-
-def populate_stats(dataset):
-    meta = {}
-    for lambda_val, reps in dataset.items():
-        if f'merged_mean' not in meta:
-            meta[f'merged_mean'] = {}
-
-        # flatten the list
-        merged_replicas = []
-        for rep in reps:
-            # merged_replicas.extend(rep)
-            # break
-            # use Chodera's EQ code to only extract the prod values after equilibration
-            merged_replicas.extend(choder_get_eqpart(rep))
-        # item[f'merged_{itype}_sem'] = sem(merged_replicas)
-        meta[f'merged_mean'][lambda_val] = np.mean(merged_replicas)
-        # item[f'merged_{itype}_std'] = np.std(merged_replicas)
-        # print('sem is overall', item['merged_avdw_sem'])
-    return meta
-
-
-def get_energies(location):
     # take only the lambda directories
     lambda_dirs = filter(lambda d: d.is_dir(), Path(location).glob(r'lambda_[0-1].[0-9]*'))
     # sort lambda directories in the increasing order
@@ -64,15 +38,6 @@ def get_energies(location):
             # partition1 is appearing
             # partition2 is disappearing
 
-            # COLUMNS:
-            # 5 is AVGELECT1
-            # 7 is AVGVDW1
-            # 11 is AVGELECT2
-            # 13 is AVGVDW2
-            # we take the last row because these are the running averages computed by NAMD
-            energies = np.loadtxt(prod_alch, comments='#', usecols=[5, 7, 11, 13])[-1]
-            energies_cum_roll = np.loadtxt(prod_alch, comments='#', usecols=[5, 7, 11, 13]).T
-
             # extract the raw datapoints
             # 4 is ELECT1
             # 6 is VDW1
@@ -80,8 +45,7 @@ def get_energies(location):
             # 12 is VDW2
             energies_datapoints = np.loadtxt(prod_alch, comments='#', usecols=[4, 6, 10, 12])
 
-            # load metadata
-            # NEW TI WINDOW: LAMBDA 0.3
+            # load metadata from the file
             with open(prod_alch) as myfile:
                 partition1, partition2 = [myfile.readline() for x in range(4)][-2:]
                 # lines that we are parsing:
@@ -89,7 +53,6 @@ def get_energies(location):
                 # PARTITION 2 SCALING: BOND 1 VDW 0.7 ELEC 0.454545
                 assert partition1.startswith('#PARTITION 1')
                 assert partition2.startswith('#PARTITION 2')
-                # fixme - use regex to get these
                 app_vdw_x = float(partition1.split('VDW')[1].split('ELEC')[0])
                 assert app_vdw_x == float(str(lambda_dir).split('_')[1])
                 dis_vdw_x = float(partition2.split('VDW')[1].split('ELEC')[0])
@@ -115,96 +78,98 @@ def get_energies(location):
     return data
 
 
-def analyse(data, location):
-    # do some processing
-    meta = {}
-    for inter_type, dataset in data.items():
-        if type(inter_type) is float:
-            continue
-        # calculate useful value for each data set
-        # calc the mean value for all the replicas merged together
-        stats = populate_stats(dataset)
-        meta[inter_type] = stats
+def choder_get_eqpart(datapoints):
+    """
+    Extracts the equilibriated part
+    """
+    [t0, g, Neff_max] = timeseries.detectEquilibration(datapoints)
+    print ('Chodera: t0 is', t0)
+    return datapoints[t0:]
 
-        continue
 
-        # get running average
-        # running_avg = pd.Series(merged_replicas).rolling(window=len(merged_replicas)).mean().iloc[len(merged_replicas) - 1:].values
-        pd_frame = pd.DataFrame({'B': merged_replicas})
-        window = int(0.1 * len(merged_replicas))
-        rolling_mean = pd_frame.rolling(window).sum() / window
-        cumu_sum = accumulate(merged_replicas)
-        cum_rolling_avg = (accu / i for i, accu in enumerate(cumu_sum, 1))
-        # plt.plot(list(cum_rolling_avg), label='my calc')
-        # plt.plot(item['cumavg_avdw'][0], label='namd')
-        # plt.plot(rolling_mean, label='rolling mean 10%')
-        # plt.legend()
-        # plt.show()
+def autocorr(x):
+    result = np.correlate(x, x, mode='full')
+    return result[int(result.size / 2):]
+
+
+def get_replicas_stats(dataset, choderas_cut=False):
+    meta = {
+        'merged_mean': {},
+        'merged_sem': {},
+        'merged_std': {},
+    }
+    for lambda_val, replicas in dataset.items():
+        # extract all the data points
+        merged_replicas = []
+        for rep in replicas:
+            if choderas_cut:
+                merged_replicas.extend(choder_get_eqpart(rep))
+            else:
+                merged_replicas.extend(rep)
+
+        # record the observables
+        meta['merged_mean'][lambda_val] = np.mean(merged_replicas)
+        meta['merged_sem'][lambda_val] = sem(merged_replicas)
+        meta['merged_std'][lambda_val] = np.std(merged_replicas)
+        # fixme - add other errors, add bootstrapping here
+    return meta
+
+
+def analyse(data, location, choderas_cut=False):
+    """
+    Process the timeseries from each replica
+    """
+
+    print('Choderas cut turned on:', choderas_cut)
+
+    # apply to each dataset
+    stats = {}
+    for interaction_type, dataset in data.items():
+        stats[interaction_type] = get_replicas_stats(dataset, choderas_cut=choderas_cut)
 
     # plot the average of the entire datasets now
     plt.figure()
-    avdw_means = np.array(list(meta['avdw']['merged_mean'].items())).T
+    avdw_means = np.array(list(stats['avdw']['merged_mean'].items())).T
     plt.plot(avdw_means[0], avdw_means[1], label='Appearing VdW means', linestyle='-', alpha=0.7)
-    dvdw_means = np.array(list(meta['dvdw']['merged_mean'].items())).T
+    dvdw_means = np.array(list(stats['dvdw']['merged_mean'].items())).T
     plt.plot(dvdw_means[0][::-1], dvdw_means[1], label='Disappearing VdW', linestyle='--', alpha=0.7)
-    aele_means = np.array(list(meta['aele']['merged_mean'].items())).T
+    aele_means = np.array(list(stats['aele']['merged_mean'].items())).T
     plt.plot(aele_means[0], aele_means[1], label='Appearing q', linestyle='-', alpha=0.7)
-    dele_means = np.array(list(meta['dele']['merged_mean'].items())).T
+    dele_means = np.array(list(stats['dele']['merged_mean'].items())).T
     plt.plot(dele_means[0][::-1], dele_means[1], label='Disappearing q', linestyle='--', alpha=0.7)
     plt.title(location)
     plt.legend()
     # plt.show()
     plt.savefig(location + '.png')
 
-    # integrate
+    # integrate over the means from each replica
     print(location)
-    # sort by lambda before integrating
-    # avdw_means.sort()
     # appearing vdw should have lambda values from 0 to 1,
     assert all([x < y for x, y in zip(avdw_means[0], avdw_means[0][1:])])
-    # print('avdw means sorted', avdw_means)
     avdw_int = np.trapz(avdw_means[1], x=avdw_means[0])
     print('int avdw', avdw_int)
 
-    # dvdw_means.sort()
     # disappearing vdw should have lambda values from 1 to 0,
     assert all([x > y for x, y in zip(dvdw_means[0], dvdw_means[0][1:])])
-    # print('avdw means sorted', dvdw_means)
     dvdw_int = np.trapz(dvdw_means[1], x=dvdw_means[0])
     print('int dvdw_means', dvdw_int)
 
-    # aele_means.sort()
     # appearing ele should have lambda values from 0 to 1,
     assert all([x < y for x, y in zip(aele_means[0], aele_means[0][1:])])
-    # print('avdw means sorted', aele_means)
     aele_int = np.trapz(aele_means[1], x=aele_means[0])
     print('int aele_means', aele_int)
 
-    # dele_means.sort()
     # disappearing ele should have lambda values from 1 to 0,
     assert all([x > y for x, y in zip(dele_means[0], dele_means[0][1:])])
-    # print('avdw means sorted', dele_means)
     dele_int = np.trapz(dele_means[1], x=dele_means[0])
     print('int dele_means', dele_int)
 
-    # we subtract partition 1 (appearing) from partition 2 (disappearing)
-    # this means that there is some energy change when the atoms disappear,
-    # and there is again some energy change when the atoms appear,
-
-    # we know that if the ligand2 is more energetically favourable
-    # than the sign should be -, because we are talking about the transition
-    # from l1 to l2 (left to right)
-    # therefore, we should do l1-l2,
-    # so let's go through some examples,
-    # if l1 is -10, but l2 is -12, then -10--12=2, but -12--10=-2, so it is just sign,
-
-    # return aele_scratch - dele_scratch + avdw_scratch - dvdw_scratch
-    # return (dele_int - aele_int) + (dvdw_int - avdw_int)
+    # return the final Delta G. Note that the sign in each delta G depends on the atoms contribution.
     return dele_int + aele_int + dvdw_int + avdw_int
 
-complex_all = get_energies('complex')
+complex_all = extract_energies('complex')
 complex_delta = analyse(complex_all, 'complex')
-lig_all = get_energies('lig')
+lig_all = extract_energies('lig')
 lig_delta = analyse(lig_all, 'lig')
 
 print("Delta ligand", lig_delta)
