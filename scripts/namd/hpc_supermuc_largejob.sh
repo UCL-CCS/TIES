a90a690135bb3e795fbba8e67d9634a49a6c3d00
bieniekmat <bieniekmat@gmail.com>
2020-03-27 14:30:02 +0000

    Large job for supermuc

---
+++ scripts/namd/hpc_supermuc_largejob.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+# 6,336 Thin compute nodes each with 48 cores and 96 GB memory
+# 144 Fat compute nodes each 48 cores and 768 GB memory per node
+# https://doku.lrz.de/display/PUBLIC/SuperMUC-NG
+# https://doku.lrz.de/display/PUBLIC/Job+Processing+with+SLURM+on+SuperMUC-NG
+# https://doku.lrz.de/display/PUBLIC/NAMD
+# https://doku.lrz.de/display/PUBLIC/Job+farming+with+SLURM
+
+#SBATCH --job-name="namd"
+#Output and error (also --output, --error):
+#SBATCH -o ./%x.%j.out
+#SBATCH -e ./%x.%j.err
+#Initial working directory (also --chdir):
+#SBATCH -D ./
+#SBATCH --time=23:20:00
+#SBATCH --no-requeue
+
+#SBATCH --nodes=3
+#SBATCH --ntasks-per-node=48
+
+#SBATCH --export=NONE
+#SBATCH --get-user-env
+#SBATCH --account=pn98ve
+#SBATCH --partition=micro # test, micro, general, large or fat
+#========================================
+module load slurm_setup
+module load namd
+
+TASKS_PER_JOB=48
+NODES_PER_JOB=1
+
+function schedule_system() {
+    echo "system $1"
+	echo "Lambda $2"
+	echo "Replica $3"
+	echo "Scheduling $1 lambda $2 replica $3"
+	(
+	    srun -N $NODES_PER_JOB -n $TASKS_PER_JOB namd2 min.namd > min.log &&
+        srun -N $NODES_PER_JOB -n $TASKS_PER_JOB namd2 eq_step1.namd > eq_step1.log &&
+        srun -N $NODES_PER_JOB -n $TASKS_PER_JOB namd2 eq_step2.namd > eq_step2.log &&
+        srun -N $NODES_PER_JOB -n $TASKS_PER_JOB namd2 eq_step3.namd > eq_step3.log &&
+        srun -N $NODES_PER_JOB -n $TASKS_PER_JOB namd2 eq_step4.namd > eq_step4.log &&
+        srun -N $NODES_PER_JOB -n $TASKS_PER_JOB namd2 prod.namd > prod.log &&
+        echo "Finished protocol for $1 lambda $2 replica $3"
+    ) &
+}
+
+# schedule the ligands
+root_dir=lig
+cd root_dir
+    for L in lambda*/ ; do
+        cd $L
+            for R in rep*/ ; do
+                cd $R
+                    schedule_system $root_dir $L $R
+                cd ..
+            done
+        cd ..
+    done
+cd ..
+
+# wait for all of them to finish
+wait
