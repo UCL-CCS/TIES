39f9c01418f6f4653b101fa63036b583d876d144
bieniekmat <bieniekmat@gmail.com>
2020-04-04 20:26:26 +0100

    Corrected errors in the supermuc big submit job script.

--- scripts/namd/hpc_supermuc_large_both.sh
+++ scripts/namd/hpc_supermuc_large_both.sh
@@ -17,7 +17,7 @@
 #SBATCH --mail-user=bieniekmat@gmail.com
 #SBATCH --no-requeue
 
-#SBATCH --time=20:30:00
+#SBATCH --time=22:30:00
 #SBATCH --nodes=65
 #SBATCH --ntasks-per-node=48
 
@@ -34,36 +34,42 @@ module load namd
 
 TASKS_PER_JOB=48
 NODES_PER_JOB=1
-
+BASE_DIR=`pwd`
 
 function schedule_system() {
-	lambda=$1
-	replica=$2
+    local lambda=$1
+	local replica=$2
+
 	echo "Scheduling ligand and complex for lambda $lambda replica $replica"
-	LIG_PATH="lig/lambda_%lambda/rep%replica"
-	COMPLEX_PATH="complex/lambda_%lambda/rep%replica"
+	LIG_PATH="lig/lambda_$lambda/rep$replica"
+	COMPLEX_PATH="complex/lambda_$lambda/rep$replica"
 	# We use timeout to ensure that each NAMD ligand process is terminated in case it gets stuck
     # 60 seconds * 60 minutes * 4 hours = 4 hours timeout for the ligand
     local LIG_TIMEOUT=$(( 60 * 60 * 4 ))
+
+    # move to the correct ligand directory
+    cd $BASE_DIR
+    cd $LIG_PATH
 	(
 
 	    # first is the ligand part with the timeout
-	    (timeout -v $LOG_TIMEOUT srun -N $NODES_PER_JOB -n $TASKS_PER_JOB namd2 $LIG_PATH/min.namd > $LIG_PATH/min.log &&
-        srun -N $NODES_PER_JOB -n $TASKS_PER_JOB namd2 $LIG_PATH/eq_step1.namd > $LIG_PATH/eq_step1.log &&
-        srun -N $NODES_PER_JOB -n $TASKS_PER_JOB namd2 $LIG_PATH/eq_step2.namd > $LIG_PATH/eq_step2.log &&
-        srun -N $NODES_PER_JOB -n $TASKS_PER_JOB namd2 $LIG_PATH/eq_step3.namd > $LIG_PATH/eq_step3.log &&
-        srun -N $NODES_PER_JOB -n $TASKS_PER_JOB namd2 $LIG_PATH/eq_step4.namd > $LIG_PATH/eq_step4.log &&
-        srun -N $NODES_PER_JOB -n $TASKS_PER_JOB namd2 $LIG_PATH/prod.namd > $LIG_PATH/prod.log &&
+	    ( timeout $LIG_TIMEOUT srun -N $NODES_PER_JOB -n $TASKS_PER_JOB namd2 min.namd > min.log &&
+        srun -N $NODES_PER_JOB -n $TASKS_PER_JOB namd2 eq_step1.namd > eq_step1.log &&
+        srun -N $NODES_PER_JOB -n $TASKS_PER_JOB namd2 eq_step2.namd > eq_step2.log &&
+        srun -N $NODES_PER_JOB -n $TASKS_PER_JOB namd2 eq_step3.namd > eq_step3.log &&
+        srun -N $NODES_PER_JOB -n $TASKS_PER_JOB namd2 eq_step4.namd > eq_step4.log &&
+        srun -N $NODES_PER_JOB -n $TASKS_PER_JOB namd2 prod.namd > prod.log &&
         echo "Finished ligand: lambda $lambda replica $replica"
         ) ;
         (
         # then the complex part, no timeout
-        srun -N $NODES_PER_JOB -n $TASKS_PER_JOB namd2 $COMPLEX_PATH/min.namd > $COMPLEX_PATH/min.log &&
-        srun -N $NODES_PER_JOB -n $TASKS_PER_JOB namd2 $COMPLEX_PATH/eq_step1.namd > $COMPLEX_PATH/eq_step1.log &&
-        srun -N $NODES_PER_JOB -n $TASKS_PER_JOB namd2 $COMPLEX_PATH/eq_step2.namd > $COMPLEX_PATH/eq_step2.log &&
-        srun -N $NODES_PER_JOB -n $TASKS_PER_JOB namd2 $COMPLEX_PATH/eq_step3.namd > $COMPLEX_PATH/eq_step3.log &&
-        srun -N $NODES_PER_JOB -n $TASKS_PER_JOB namd2 $COMPLEX_PATH/eq_step4.namd > $COMPLEX_PATH/eq_step4.log &&
-        srun -N $NODES_PER_JOB -n $TASKS_PER_JOB namd2 $COMPLEX_PATH/prod.namd > $COMPLEX_PATH/prod.log &&
+        cd $BASE_DIR && cd $COMPLEX_PATH &&
+        srun -N $NODES_PER_JOB -n $TASKS_PER_JOB namd2 min.namd > min.log &&
+        srun -N $NODES_PER_JOB -n $TASKS_PER_JOB namd2 eq_step1.namd > eq_step1.log &&
+        srun -N $NODES_PER_JOB -n $TASKS_PER_JOB namd2 eq_step2.namd > eq_step2.log &&
+        srun -N $NODES_PER_JOB -n $TASKS_PER_JOB namd2 eq_step3.namd > eq_step3.log &&
+        srun -N $NODES_PER_JOB -n $TASKS_PER_JOB namd2 eq_step4.namd > eq_step4.log &&
+        srun -N $NODES_PER_JOB -n $TASKS_PER_JOB namd2 prod.namd > prod.log &&
         echo "Finished complex: lambda $lambda replica $replica"
         )
     ) &
