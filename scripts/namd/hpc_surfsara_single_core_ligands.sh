f0a079755476d771e88f8d80fc0a43a433396c4e
bieniekmat <bieniekmat@gmail.com>
2020-04-09 14:22:40 +0100

    Submitter that runs ligands with just 1 core each.

---
+++ scripts/namd/hpc_surfsara_single_core_ligands.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+# just one normal node
+##SBATCH -N 1        # request 1 nodes
+##SBATCH -p short    # request partition 'short', see below
+#SBATCH -n 1       # request 40 processes (each runs on 1 core), mostly to run MPI programs on
+                    # SLURM will compute the number of nodes needed
+##SBATCH -n 16 -c 4  # request 16*4 cores, an MPI program will start
+                    # 16 processes, each process can spawn 4 OpenMP
+		    # threads. (the environment variable OMP_NUM_THREADS
+		    # will be set to 4)
+##SBATCH -N 6 --ntasks-per-node=4
+                    # An MPI program will start 4 processes on 6 nodes each
+		    # 24 processes in total
+##SBATCH -t 2:00:00  # The job can take at most 2 wall-clock hours.
+##SBATCH -t 10       # 10 minutes
+##SBATCH -t 10:20    # 10 minutes plus 20 seconds
+##SBATCH -t 23:20:30 #  10 hours plus 20 minutes plus 30 seconds
+#SBATCH -t 2-00:00:00 #  10 hours plus 20 minutes plus 30 seconds
+
+module load 2019
+module load NAMD/2.13-foss-2018b-mpi
+#module load pre2019
+#module load NAMD/2.12-foss-2017b-mpi
+
+# request specifically 65 processes (2 for test)
+# energy minimisation
+namd2 min.namd > min.log
+namd2 eq_step1.namd > eq_step1.log
+namd2 eq_step2.namd > eq_step2.log
+namd2 eq_step3.namd > eq_step3.log
+namd2 eq_step4.namd > eq_step4.log
+# run the production
+namd2 prod.namd > prod.log
