8f6846efabc4ceb594720f21d2d8f307db23c6aa
bieniekmat <bieniekmat@gmail.com>
2020-04-29 18:11:27 +0100

    A more general structure

--- scripts/ambertools/assign_charge_parameters_antechamber.sh
+++ scripts/ambertools/assign_charge_parameters_antechamber.sh
@@ -11,5 +11,5 @@ cd $CURRENT_DIR
 echo 'Current Dir' $CURRENT_DIR
 
 # generate the mol2 topology
-antechamber -i $LEFT.pdb -fi pdb -o $LEFT.mol2 -fo mol2 -c bcc -at gaff2 -nc {net_charge} -dr n  > ante_left.log
-antechamber -i $RIGHT.pdb -fi pdb -o $RIGHT.mol2 -fo mol2 -c bcc -at gaff2 -nc {net_charge} -dr n > ante_right.log
+antechamber -i $LEFT -fi pdb -o left.mol2 -fo mol2 -c bcc -at gaff2 -nc {net_charge} -dr n  > ante_left.log
+antechamber -i $RIGHT -fi pdb -o right.mol2 -fo mol2 -c bcc -at gaff2 -nc {net_charge} -dr n > ante_right.log
--- scripts/ambertools/assign_charge_parameters_parmchk2.sh
+++ scripts/ambertools/assign_charge_parameters_parmchk2.sh
@@ -13,8 +13,8 @@ echo 'Current Dir' $CURRENT_DIR
 # note that the charge -nc -1 so that you have to know the charges ahead
 # QUESTION: is there an automatic charge detection? 
 # this takes a minute or two
-parmchk2 -i $LEFT.mol2 -o $LEFT.frcmod -f mol2 -s gaff2
-parmchk2 -i $RIGHT.mol2 -o $RIGHT.frcmod -f mol2 -s gaff2
+parmchk2 -i left.mol2 -o left.frcmod -f mol2 -s gaff2
+parmchk2 -i right.mol2 -o right.frcmod -f mol2 -s gaff2
 # this goes through the mol2 previously generated and adds any terms in the force field that are missing (angles, dihedrals, etc) 
 # So we have now the terms and the charges and the bonds.
 # The mol2 has the right atom type so that is directly connected the gaff2,
