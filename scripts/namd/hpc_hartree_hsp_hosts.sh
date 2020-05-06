fbb7f49d223b75906245d7f590e21360d5be2397
bieniekmat <bieniekmat@gmail.com>
2020-05-06 13:50:21 +0100

    The full simulation run.

--- scripts/namd/hpc_hartree_hsp_hosts.sh
+++ scripts/namd/hpc_hartree_hsp_hosts.sh
@@ -5,15 +5,14 @@
 #BSUB -R "span[ptile=32]" ###### 32 cores per node
 # 64 cpu * 65 nodes
 # this should be ptile * number of nodes,
-#BSUB -n 256       ##### for 64 cores, total nodes requested 2
+#BSUB -n 8320       ##### for 64 cores, total nodes requested 2, 8320 is 128*65
 #BSUB -q compbiomed
 #BSUB -W 00:50
 #BSUB -x
 
 ROOT_WORK=$HCBASE/tyk2_l5_l16
 cd $ROOT_WORK
-# cores per simulation
-NP=128
+NP=128 # cores per simulation
 HOSTS_PER_SIM=4 # numer of hosts per simulation
 LIG_TIMEOUT=$(( 60 * 60 * 10 )) # time out of the ligand simulations: 10 hours
 
@@ -23,10 +22,10 @@ source /etc/profile.d/modules.sh
 module load namd-gcc/2.12
 
 # generate all the simulations you want to run:
-# declare -a lambdas=(0.00 0.05 0.10 0.20 0.30 0.40 0.50 0.60 0.70 0.80 0.90 0.95 1.00)
-# declare -a replicas=(1 2 3 4 5)
-declare -a lambdas=(0.50)
-declare -a replicas=(1 2)
+declare -a lambdas=(0.00 0.05 0.10 0.20 0.30 0.40 0.50 0.60 0.70 0.80 0.90 0.95 1.00)
+declare -a replicas=(1 2 3 4 5)
+# declare -a lambdas=(0.50) # testing
+# declare -a replicas=(1 2) # testing
 echo "Number lambdas: ${#lambdas[@]}"
 echo "Number replicas: ${#replicas[@]}"
 
@@ -86,9 +85,10 @@ for sim_no in $(seq 1 $SIM_NO); do
 	# run the next simulation
 	export I_MPI_PIN=yes
     (
-    	cd $ROOT_WORK/lig/$SIM &&
+
 	    # first is the ligand part with the timeout
-	    ( timeout $LIG_TIMEOUT mpiexec -genv I_MPI_PIN_DOMAIN=auto:compact -n $NP -hosts $HOSTGROUP namd2 +pemap 1-31 +commap 0 min.namd > min.log &&
+	    ( cd $ROOT_WORK/lig/$SIM &&
+	    timeout $LIG_TIMEOUT mpiexec -genv I_MPI_PIN_DOMAIN=auto:compact -n $NP -hosts $HOSTGROUP namd2 +pemap 1-31 +commap 0 min.namd > min.log &&
         mpiexec -genv I_MPI_PIN_DOMAIN=auto:compact -n $NP -hosts $HOSTGROUP namd2 +pemap 1-31 +commap 0 eq_step1.namd > eq_step1.log &&
         mpiexec -genv I_MPI_PIN_DOMAIN=auto:compact -n $NP -hosts $HOSTGROUP namd2 +pemap 1-31 +commap 0 eq_step2.namd > eq_step2.log &&
         mpiexec -genv I_MPI_PIN_DOMAIN=auto:compact -n $NP -hosts $HOSTGROUP namd2 +pemap 1-31 +commap 0 eq_step3.namd > eq_step3.log &&
