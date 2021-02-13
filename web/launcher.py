935e0ac118ca2f9d24370f8fdb3e1884adca12fb
bieniekmat <bieniekmat@gmail.com>
2021-02-13 00:17:34 +0000

    The full version rearranged

--- web/launcher.py
+++ web/launcher.py
@@ -7,7 +7,7 @@ import os
 
 from flask import Flask, render_template, request, redirect, send_file
 
-def create_app(storageties, ambertools, load_ties):
+def create_app(storageties, tiesenv):
     logger = logging.getLogger('waitress')
     logger.setLevel(logging.INFO)
 
@@ -19,13 +19,9 @@ def create_app(storageties, ambertools, load_ties):
 
     @app.route('/test')
     def test():
-        return 'simple'
+        return 'simple test'
 
-    @app.route('/')
-    def hello_world():
-        return render_template('main.html')
-
-    @app.route("/upload-image", methods=["GET", "POST"])
+    @app.route("/", methods=["GET", "POST"])
     def upload_image():
         if request.method == "POST":
             # if request.form['password'] != 'haslo':
@@ -52,39 +48,31 @@ def create_app(storageties, ambertools, load_ties):
 
             # create a dedicated directory (date and metadata) for the request where to save the files etc
             session_dir = work_dir / f'{datetime.datetime.now().strftime("%d-%m-%Y-%H:%M:%S:%f")}'
-            # todo - save IP address, and other info related in the request (entire request?)
             session_dir.mkdir()
+            # todo - save IP address, and other info related in the request (entire request?)
 
             # save the files
             request.files['ligand_ini'].save(session_dir / request.files['ligand_ini'].filename)
             request.files['ligand_fin'].save(session_dir / request.files['ligand_fin'].filename)
 
-            # redirect?
-            # run TIES on the underlying system
-            # activate ties env
-            loadties = load_ties
-            # run it
-            source_amber = "export AMBERHOME=/home/ccsadmin/anaconda3 ; "
-            command = f'{source_amber} /home/ccsadmin/anaconda3/bin/ties create ' \
-                      f'-l {request.files["ligand_ini"].filename} ' \
-                      f'{request.files["ligand_fin"].filename} ' \
+            command = f'{tiesenv} ties create ' \
+                      f'-l {request.files["ligand_ini"].filename} {request.files["ligand_fin"].filename} ' \
                       f'-nc {net_charge}'
-            print(f'About to try the command: {command}')
-            with open(session_dir / 'run.log', 'w') as LOG:
-                output = subprocess.run([command], shell=True, stdout=LOG, stderr=LOG, cwd=session_dir)
-            # todo apply the /n to be actually not escaped characters?
-            print('OUTPUT', output)
+            print(f'About to run the command: {command}')
+            with open(session_dir / 'ties20.log', 'w') as LOG:
+                subprocess.run([command], shell=True, stdout=LOG, stderr=LOG, cwd=session_dir)
 
-            # zip altogether
+            # zip the output
             os.chdir(session_dir)
             with zipfile.ZipFile(session_dir / 'ties20.zip', 'w') as myzip:
                 for i in (session_dir / 'ties20').glob('**/*'):
                     print('rel', i.relative_to((session_dir / 'ties20')))
                     myzip.write(i.relative_to(session_dir))
                 # add the main log
-                myzip.write('run.log')
+                myzip.write('ties20.log')
             zipped_output = session_dir / 'ties20.zip'
 
+            # send the file back
             return send_file(zipped_output, as_attachment=True,
                              attachment_filename=f'ties20_{request.files["ligand_ini"].filename}_{request.files["ligand_fin"].filename}.zip')
 
