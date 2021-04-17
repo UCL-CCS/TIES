2eb0f25ba30410f06bb9627708d42010eaf5d08c
bieniekmat <bieniekmat@gmail.com>
2021-04-17 18:34:36 +0100

    Minimum API. Rearranged writers to be a function of the suptop.

--- examples/mol2_2files/api.py
+++ examples/mol2_2files/api.py
@@ -2,9 +2,9 @@ from ties import Morph
 
 morph = Morph('MCL1_lig_02.mol2', 'MCL1_lig_03.mol2')
 morph.make_atom_names_unique()
-suptop = morph.compute_suptop()
+hybrid = morph.compute_suptop()
 
 # save meta data
-morph.write_summary_json()
-morph.write_pdb()
-morph.write_hybrid_mol2()
+hybrid.write_summary_json()
+hybrid.write_pdb()
+hybrid.write_mol2()
--- examples/mol2_2files/min_api.py
+++ examples/mol2_2files/min_api.py
@@ -1,9 +1,9 @@
 from ties import Morph
 
 morph = Morph('MCL1_lig_02.mol2', 'MCL1_lig_03.mol2')
-suptop = morph.compute_suptop()
+hybrid = morph.compute_suptop()
 
-# save meta data
-morph.write_summary_json()
-morph.write_pdb()
-morph.write_hybrid_mol2()
+# save the results
+hybrid.write_summary_json()
+hybrid.write_pdb()
+hybrid.write_mol2()
