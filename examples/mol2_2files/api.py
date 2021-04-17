c9aff8fc5b9a61ece108c04505fb4c7339825f85
bieniekmat <bieniekmat@gmail.com>
2021-04-17 17:15:09 +0100

    Adding a simple python example of the API

---
+++ examples/mol2_2files/api.py
@@ -0,0 +1,10 @@
+from ties import Morph
+
+morph = Morph('MCL1_lig_02.mol2', 'MCL1_lig_03.mol2')
+morph.make_atom_names_unique()
+suptop = morph.compute_suptop()
+
+# save meta data
+morph.write_summary_json()
+morph.write_pdb()
+morph.write_hybrid_mol2()
