a575db3d40318ee414fe9f5f0da61873a9843ee6
bieniekmat <bieniekmat@gmail.com>
2021-05-03 18:12:36 +0100

    Protein test and check added.

---
+++ examples/mol2_2ligands_MCL1/.test_CMD_with_protein.py
@@ -0,0 +1,13 @@
+# verify that the min_api.py worked fine
+import glob
+import pathlib
+
+# the default directory was created
+tiesdir = pathlib.Path('cmdties_protein')
+assert tiesdir.exists()
+
+# the files have been saved
+assert (tiesdir / 'fep_l02_l03.json').exists()
+assert (tiesdir / 'l02_l03_morph.mol2').exists()
+assert (tiesdir / 'l02_l03_morph.pdb').exists()
+assert (tiesdir / 'ties-l02-l03' / 'com' / 'build' / 'sys_solv.top' ).exists()
