29191ade95fe87e3d0894f5ff976eca3678aa2bb
bieniekmat <bieniekmat@gmail.com>
2022-01-23 22:45:14 +0000

    Protein example

--- examples/mol2_2ligands_MCL1/api.py
+++ examples/mol2_2ligands_MCL1/api.py
@@ -1,6 +1,10 @@
 from ties import Pair
+from ties import Config
+from ties.protein import Protein
 
-pair = Pair('l02.mol2', 'l03.mol2', ligand_net_charge=-1)
+config = Config()
+config.workdir = 'ties20'
+pair = Pair('l02.mol2', 'l03.mol2', ligand_net_charge=-1, config=config)
 pair.make_atom_names_unique()
 
 hybrid = pair.superimpose()
@@ -11,3 +15,7 @@ hybrid.write_pdb()
 hybrid.write_mol2()
 
 hybrid.prepare_inputs()
+
+config.protein = 'protein.pdb'
+protein = Protein(config.protein, config)
+hybrid.prepare_inputs(protein=protein)
