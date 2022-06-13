d749dfd3df5f39b35f32ef4e759c1621c94f0e82
bieniekmat <bieniekmat@gmail.com>
2022-06-13 19:27:14 +0100

    Adapting pyqcprot package into ties for the fast rotation_matrix qith QCP.

---
+++ testing/test_transformation.py
@@ -0,0 +1,66 @@
+"""
+The tests focus on the transformation related to the RMSD calculation and the superimposition of molecules using the
+adapted qcp package.
+"""
+import numpy
+
+from ties.transformation import superimpose_coordinates, rmsd_with_rotation
+
+
+def test_transformation_default():
+    """
+    This test was extracted from the pyqcprot package.
+    The reference value was provided for this dataset.
+    """
+    ref_coords = numpy.zeros((7, 3), dtype=numpy.float64)
+    mob_coords = numpy.zeros((7, 3), dtype=numpy.float64)
+
+    # create coordinates with the [atomID][xyz]
+    ref_coords[0][0] = -2.803
+    ref_coords[0][1] = -15.373
+    ref_coords[0][2] = 24.556
+    ref_coords[1][0] = 0.893
+    ref_coords[1][1] = -16.062
+    ref_coords[1][2] = 25.147
+    ref_coords[2][0] = 1.368
+    ref_coords[2][1] = -12.371
+    ref_coords[2][2] = 25.885
+    ref_coords[3][0] = -1.651
+    ref_coords[3][1] = -12.153
+    ref_coords[3][2] = 28.177
+    ref_coords[4][0] = -0.440
+    ref_coords[4][1] = -15.218
+    ref_coords[4][2] = 30.068
+    ref_coords[5][0] = 2.551
+    ref_coords[5][1] = -13.273
+    ref_coords[5][2] = 31.372
+    ref_coords[6][0] = 0.105
+    ref_coords[6][1] = -11.330
+    ref_coords[6][2] = 33.567
+
+    mob_coords[0][0] = -14.739
+    mob_coords[0][1] = -18.673
+    mob_coords[0][2] = 15.040
+    mob_coords[1][0] = -12.473
+    mob_coords[1][1] = -15.810
+    mob_coords[1][2] = 16.074
+    mob_coords[2][0] = -14.802
+    mob_coords[2][1] = -13.307
+    mob_coords[2][2] = 14.408
+    mob_coords[3][0] = -17.782
+    mob_coords[3][1] = -14.852
+    mob_coords[3][2] = 16.171
+    mob_coords[4][0] = -16.124
+    mob_coords[4][1] = -14.617
+    mob_coords[4][2] = 19.584
+    mob_coords[5][0] = -15.029
+    mob_coords[5][1] = -11.037
+    mob_coords[5][2] = 18.902
+    mob_coords[6][0] = -18.577
+    mob_coords[6][1] = -10.001
+    mob_coords[6][2] = 17.996
+
+    # compute the rotational matrix
+    rot, rmsd_value, rmsd_applied_rot_value, mob_coords = superimpose_coordinates(ref_coords, mob_coords)
+
+    numpy.testing.assert_allclose([rmsd_applied_rot_value], [0.719106], rtol=1e-05)
---
+++ ties/transformation.py
@@ -0,0 +1,153 @@
+"""
+
+"""
+
+import copy
+
+import numpy
+import pyqcprot as qcp
+
+
+def test():
+    # Setup coordinates
+
+    frag_a = numpy.zeros((3, 7), dtype=numpy.float64)
+    frag_b = numpy.zeros((3, 7), dtype=numpy.float64)
+    N = 7
+
+    frag_a[0][0] = -2.803
+    frag_a[1][0] = -15.373
+    frag_a[2][0] = 24.556
+    frag_a[0][1] = 0.893
+    frag_a[1][1] = -16.062
+    frag_a[2][1] = 25.147
+    frag_a[0][2] = 1.368
+    frag_a[1][2] = -12.371
+    frag_a[2][2] = 25.885
+    frag_a[0][3] = -1.651
+    frag_a[1][3] = -12.153
+    frag_a[2][3] = 28.177
+    frag_a[0][4] = -0.440
+    frag_a[1][4] = -15.218
+    frag_a[2][4] = 30.068
+    frag_a[0][5] = 2.551
+    frag_a[1][5] = -13.273
+    frag_a[2][5] = 31.372
+    frag_a[0][6] = 0.105
+    frag_a[1][6] = -11.330
+    frag_a[2][6] = 33.567
+
+    frag_b[0][0] = -14.739
+    frag_b[1][0] = -18.673
+    frag_b[2][0] = 15.040
+    frag_b[0][1] = -12.473
+    frag_b[1][1] = -15.810
+    frag_b[2][1] = 16.074
+    frag_b[0][2] = -14.802
+    frag_b[1][2] = -13.307
+    frag_b[2][2] = 14.408
+    frag_b[0][3] = -17.782
+    frag_b[1][3] = -14.852
+    frag_b[2][3] = 16.171
+    frag_b[0][4] = -16.124
+    frag_b[1][4] = -14.617
+    frag_b[2][4] = 19.584
+    frag_b[0][5] = -15.029
+    frag_b[1][5] = -11.037
+    frag_b[2][5] = 18.902
+    frag_b[0][6] = -18.577
+    frag_b[1][6] = -10.001
+    frag_b[2][6] = 17.996
+
+    # Allocate rotation array
+    rot = numpy.zeros((9,), dtype=numpy.float64)
+
+    # Calculate center of geometry
+    comA = numpy.sum(frag_a, axis=1) / N
+    comB = numpy.sum(frag_b, axis=1) / N
+
+    # Center each fragment
+    frag_a = frag_a - comA.reshape(3, 1)
+    frag_b = frag_b - comB.reshape(3, 1)
+
+    # Calculate rmsd and rotation matrix
+    rmsd = qcp.CalcRMSDRotationalMatrix(frag_a, frag_b, N, rot, None)
+
+    print('qcp rmsd = ', rmsd)
+    print('rotation matrix:')
+    print(rot.reshape((3, 3)))
+
+
+    # Calculate rmsd after applying rotation
+    def rmsd(a, b):
+        """Returns RMSD between two coordinate sets a and b."""
+        return numpy.sqrt(numpy.sum(numpy.power(a - b, 2)) / a.shape[1])
+
+
+    # rotate frag_b to obtain optimal alignment
+    frag_br = frag_b.T * numpy.matrix(rot.reshape((3, 3)))
+    rmsd = rmsd(frag_br.T, frag_a)
+    print('rmsd after applying rotation: ', rmsd)
+
+
+# Calculate rmsd after applying rotation
+def get_rmsd(a, b):
+    """Returns RMSD between two coordinate sets a and b."""
+    return numpy.sqrt(numpy.sum(numpy.power(a - b, 2)) / a.shape[1])
+
+
+def rmsd_with_rotation(pos_ref, pos_mob, rot):
+    # rotate frag_b to obtain optimal alignment
+    pos_mob_br = pos_mob.T * numpy.matrix(rot.reshape((3, 3)))
+    # calc rmsd
+    overlap_rmsd = get_rmsd(pos_mob_br.T, pos_ref)
+    print('rmsd after applying rotation: ', overlap_rmsd)
+    return overlap_rmsd
+
+
+def superimpose_coordinates(ref, mob):
+    """
+    Superimpose the mob structure to the ref structure and return the necessary rotational matrix etc.
+
+    :param ref: coordinates for the reference structure
+    :param mob: coordaintes for the mobile structure
+    :return: (rot, rmsd, overlap_rmsd) with rot being a rotational matrix that has to be applied to the
+        mobile structure in order to superimpose it with the reference structure.
+        Overlap_rmsd is the rmsd value obtained after applying the rotational matrix.
+    """
+    if len(ref) != len(mob):
+        raise Exception('Coordinates to be overlapped must have the same number of atoms')
+    if ref.shape[1] != mob.shape[1] != 3:
+        raise Exception('The coordinates should be have the array shape (n, 3)')
+
+    N = len(ref)
+
+    ref = copy.deepcopy(ref.T)
+    mob_coords = copy.deepcopy(mob.T)
+
+    # Allocate rotation array
+    rotation_matrix = numpy.zeros((9,), dtype=numpy.float64)
+
+    # Calculate center of geometry
+    comA = numpy.sum(ref, axis=1) / N
+    comB = numpy.sum(mob_coords, axis=1) / N
+
+    # Center each fragment
+    ref = ref - comA.reshape(3, 1)
+    mob_origin = mob_coords - comB.reshape(3, 1)
+
+    # Calculate rmsd and rotation matrix
+    rmsd_before_superimposition = qcp.CalcRMSDRotationalMatrix(ref, mob_origin, N, rotation_matrix, None)
+
+    # print('qcp rmsd = ', rmsd)
+    # print('rotation matrix:')
+    # print(rot.reshape((3, 3)))
+
+    #
+    # apply the rotation and
+    rmsd_after_superimposition = rmsd_with_rotation(ref, mob_origin, rotation_matrix)
+
+    return rotation_matrix, \
+           rmsd_before_superimposition, \
+           rmsd_after_superimposition, \
+           mob_coords
