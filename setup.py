4a19e60ae4f3b78b2f9f1420cd4fed8d353000cf
bieniekmat <bieniekmat@gmail.com>
2022-06-17 10:23:54 +0100

    added the missing git files

--- setup.py
+++ setup.py
@@ -11,15 +11,15 @@ except AttributeError:
     numpy_include = numpy.get_numpy_include()
 
 # handle cython modules: pyqcprot module for the rotation matrix
-#try:
-#    from Cython.Distutils import build_ext
-#    use_cython = True
-#    cmdclass = {'build_ext': build_ext}
-#except ImportError:
-use_cython = False
-cmdclass = {}
-#finally:
-#    print (f'use_cython: {use_cython}')
+try:
+    from Cython.Distutils import build_ext
+    use_cython = True
+    cmdclass = {'build_ext': build_ext}
+except ImportError:
+    use_cython = False
+    cmdclass = {}
+finally:
+    print (f'use_cython: {use_cython}')
 
 ext_modules = [Extension("ties/pyqcprot", [f"ties/pyqcprot/pyqcprot.{'pyx' if use_cython else 'c'}"],
                          include_dirs=[numpy_include],
@@ -35,6 +35,7 @@ setup(
     author_email='bieniekmat@gmail.com',
     packages=find_packages(),
     include_package_data=True,
+    install_requires=('dwave_networkx', 'numpy'),
     entry_points={
         'console_scripts': [
             'ties = ties:cmdties.command_line_script'
