b34f08a581130e072846d80b74c0e7aff09e157b
bieniekmat <bieniekmat@gmail.com>
2022-08-06 20:29:59 +0100

    Making the Protein interface more similar to Ligand to allow Protein(config=config) which is more succinct.

--- ties/protein.py
+++ ties/protein.py
@@ -1,6 +1,6 @@
 
 import ties.generator
-
+from ties.config import Config
 
 class Protein:
     """
@@ -12,9 +12,20 @@ class Protein:
     :param config: Optional configuration for the protein
     :type config: :class:`Config`
     """
-    def __init__(self, filename, config):
-        self.file = filename
-        self.config = config
+    def __init__(self, filename=None, config=None):
+        if filename is None and config is None:
+            raise Exception('Protein filename is not passed and the config file is missing. ')
+
+        self.config = Config() if config is None else config
+
+        if filename is None:
+            if config.protein is None:
+                raise Exception('Could not find the protein in the config object. ')
+            self.file = config.protein
+        elif filename is not None:
+            self.file = filename
+            # update the config
+            config.protein = filename
 
         # fixme - check if the file exists at this point, throw an exception otherwise
 
