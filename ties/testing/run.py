e70e49285c5f189927efb57203f93033dfbac692
adw62 <alexw1243@gmail.com>
2022-10-07 14:34:36 +0100

    add run test py

---
+++ ties/testing/run.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import pytest, tempfile, os, site, shutil
+
+def main():
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        print('created temporary directory', tmpdirname)
+        test_dir = os.path.join(site.getsitepackages()[0], 'ties', 'testing')
+        tmp_test_dir = os.path.join(tmpdirname, 'ties', 'unit_testing', 'testing')
+        shutil.copytree(test_dir, tmp_test_dir)
+        pytest.main([os.path.join(tmp_test_dir)])
+
+if __name__ == '__main__':
+    main()
