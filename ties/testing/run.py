13674c041ef33870a14221c846bde7a4dd019d3c
bieniekmat <bieniekmat@gmail.com>
2023-04-05 17:29:22 +0100

    Using sys exit to pass the results to the main process

--- ties/testing/run.py
+++ ties/testing/run.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import pytest, tempfile, os, site, shutil
+import pytest, tempfile, os, sys, site, shutil
 
 def main():
     with tempfile.TemporaryDirectory() as tmpdirname:
@@ -8,7 +8,7 @@ def main():
         test_dir = os.path.join(site.getsitepackages()[0], 'ties', 'testing')
         tmp_test_dir = os.path.join(tmpdirname, 'ties', 'unit_testing', 'testing')
         shutil.copytree(test_dir, tmp_test_dir)
-        return pytest.main([os.path.join(tmp_test_dir)])
+        sys.exit(pytest.main([os.path.join(tmp_test_dir)]))
 
 if __name__ == '__main__':
     main()
