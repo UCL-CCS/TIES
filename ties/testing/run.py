f25d664f5ca2f464aa43cc1e3032f747ab3517d4
bieniekmat <bieniekmat@gmail.com>
2023-04-05 16:32:59 +0100

    returning pytest results

--- ties/testing/run.py
+++ ties/testing/run.py
@@ -8,7 +8,7 @@ def main():
         test_dir = os.path.join(site.getsitepackages()[0], 'ties', 'testing')
         tmp_test_dir = os.path.join(tmpdirname, 'ties', 'unit_testing', 'testing')
         shutil.copytree(test_dir, tmp_test_dir)
-        pytest.main([os.path.join(tmp_test_dir)])
+        return pytest.main([os.path.join(tmp_test_dir)])
 
 if __name__ == '__main__':
     main()
