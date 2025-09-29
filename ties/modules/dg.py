7edb2128ae862aa9ca8a5d4a60db2e6014242998
bieniekmat <bieniekmat@gmail.com>
2025-09-29 15:10:55 +0100

    Cleared up tester.

--- ties/modules/dg.py
+++ ties/modules/dg.py
@@ -18,99 +18,11 @@ Then, feeding it directly into here, we should be able to recover
 the original mu centres. 
 """
 
-
 kcal_mol = unit.kilocalorie_per_mole
 
 
-def basic():
-    kcal_mol = unit.kilocalorie_per_mole
-    g = ReferenceState(label="0")
-    experimental_result1 = Measurement(
-        labelA=g,
-        labelB="CAT-13a",
-        DG=-8.83 * kcal_mol,
-        uncertainty=0.10 * kcal_mol,
-        computational=False,
-    )
-    experimental_result2 = Measurement(
-        labelA=g,
-        labelB="CAT-17g",
-        DG=-9.73 * kcal_mol,
-        uncertainty=0.10 * kcal_mol,
-        computational=False,
-    )
-    # Load/create calculated results
-    calculated_result = Measurement(
-        labelA="CAT-13a",
-        labelB="CAT-17g",
-        DG=0.36 * kcal_mol,
-        uncertainty=0.11 * kcal_mol,
-        computational=True,
-    )
-    # Incrementally created FEMap
-    fe = FEMap()
-    fe.add_measurement(experimental_result1)
-    fe.add_measurement(experimental_result2)
-    fe.add_measurement(calculated_result)
-
-    df = fe.get_absolute_dataframe()
-    print(df)
-
-
-def better():
-    g = ReferenceState()
-    experimental_result1 = Measurement(
-        labelA=g,
-        labelB="0",
-        DG=-6 * kcal_mol,
-        uncertainty=0.10 * kcal_mol,
-        computational=False,
-    )
-
-    # Incrementally created FEMap
-    fe = FEMap()
-    fe.add_measurement(experimental_result1)
-    fe.add_relative_calculation(
-        labelA="0", labelB="1", value=-1 * kcal_mol, uncertainty=0.10 * kcal_mol
-    )
-
-    fe.generate_absolute_values()
-    df = fe.get_absolute_dataframe()
-    print(df)
-
-
-def fine_but_old_interface():
-    exp_value = -6
-    exp_label = "0"
-
-    fe = FEMap()
-    fe.add_experimental_measurement(
-        exp_label, value=exp_value * kcal_mol, uncertainty=0.10 * kcal_mol
-    )
-    fe.add_relative_calculation(
-        labelA=exp_label, labelB="1", value=-1 * kcal_mol, uncertainty=0.10 * kcal_mol
-    )
-    fe.add_relative_calculation(
-        labelA="1", labelB="2", value=-1 * kcal_mol, uncertainty=0.10 * kcal_mol
-    )
-
-    fe.generate_absolute_values()
-    df = fe.get_absolute_dataframe()
-
-    # look up the experimental value to find the shift
-    exp_val_mle = df[(df["label"] == exp_label) & (df["source"] == "MLE")][
-        "DG (kcal/mol)"
-    ].values[0]
-
-    # calculate the needed shift
-    shift = exp_value - exp_val_mle
-    df["DG (kcal/mol)"] += shift
-
-    print(df)
-
-
 def new_interface():
-    exp_value = -6
+    exp_value = -6  # kcal_mol
     exp_label = "0"
 
     fe = FEMap()
@@ -146,6 +58,9 @@ def new_interface():
     fe.generate_absolute_values()
     df = fe.get_absolute_dataframe()
 
+    # cinnabar applied a shift (to 0 apparently)
+    # see #111
+
     # use the MLE value of your experimental point to calculate the shift
     exp_val_mle = df[(df["label"] == exp_label) & (df["source"] == "MLE")][
         "DG (kcal/mol)"
@@ -169,4 +84,3 @@ def from_csv_ref():
 
 
 new_interface()
-print("hi")
