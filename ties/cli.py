c4b511635e53dc726b1396e720562b8a4835755b
bieniekmat <bieniekmat@gmail.com>
2025-09-29 11:33:10 +0100

    Dropping weights.

--- ties/cli.py
+++ ties/cli.py
@@ -343,19 +343,6 @@ def command_line_script():
         default=False,
         help="Ignore the specific atom types in the superimposition. Use only the elements. ",
     )
-    parser.add_argument(
-        "-weights",
-        "--mcs-rmsd-weights",
-        metavar="str",
-        dest="weights_ratio",
-        type=ArgparseChecker.ratio,
-        required=False,
-        default=None,
-        help="The weights for the weighted sum of 1) MCS overlap size to 2) RMSD "
-        "when coordinates are used for selection of the best structure. "
-        'Default it is "1:0" for "MCS:RMSD".  '
-        "MCS is defined (1 - MCS fraction), so lower value is better.",
-    )
 
     # initialise the config class
     args = parser.parse_args()
