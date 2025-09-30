1fce97e1bb7d0a45ef0f301aefa8a51fa14b89db
bieniekmat <bieniekmat@gmail.com>
2025-09-30 10:01:57 +0100

    Fix: When using this as a module, __name__ is not "__main__". It is "ties.modules"

--- ties/modules/__init__.py
+++ ties/modules/__init__.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-if __name__ == "__main__":
+if __name__ == "ties.modules":
     modules = Path(__file__).parent.glob("*py")
 
     modules_names = {m.stem for m in modules} - {"__init__"}
